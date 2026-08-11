2a3f069ccf6a4d2d6242ab81784cac46fa698e8f
Vamshi <2427899@cognizant.com>
2026-08-11 13:13:05 +0530

    Disable TS checks for deployment

--- food_donation_frontend/src/main.tsx
+++ food_donation_frontend/src/main.tsx
@@ -1,5 +1,5 @@
 import { StrictMode } from 'react'
-import { BrowserRouter,HashRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
